a182cc4d63fba7a221ae8fd0822942eaecd8a1ac
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:17:22 +0530

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -2,6 +2,7 @@
 """
     authorization function middleware
 """
+from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/login")
 
