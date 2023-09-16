fe2dde245af97a3a89600593690747d49b060c9f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:38:12 +0530

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Form, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from models.user_model import UserInDB
-from security.authorize import authenticate_user
+from security.authorize import authenticate_user, oauth2_scheme
 from security.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from services.user_service import user_exists, get_next_avail_id, add_new_user
 
