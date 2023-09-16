c24f9b3e3207cfd00e4fc943fb1ce4aa9bdc4878
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:24:36 +0530

    Update user_dao.py

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -1,7 +1,7 @@
-
 """
     middleware for accessing the user database and performing CRUD operations on the user table
 """
+
 
 class UserDAO:
     def __init__(self, host, user, password, database):
@@ -10,7 +10,6 @@ class UserDAO:
         self.password = password
         self.database = database
         self.cnx = None
-
 
     def connect(self):
         try:
