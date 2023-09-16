7b6c8451ba10217dfb33fe28d23e888252b430e9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:40:18 +0530

    Update authorize.py

--- security/authorize.py
+++ security/authorize.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 
-from jose import JWTError
+from jose import JWTError, jwt
 
 from security.hashing import verify_password, is_token_blacklisted
 from services.user_service import get_user
