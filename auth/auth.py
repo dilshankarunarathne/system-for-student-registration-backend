a866d6415305e2b49f2c32c0b4e9a9f8c92392ac
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:15:44 +0530

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Form, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 
 """
     routers for authentication
