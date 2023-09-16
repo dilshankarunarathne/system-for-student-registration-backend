b142f6ad1df74b077990f2cfdefe9bb94d982077
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:20:45 +0530

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -15,4 +15,5 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-
+def get_password_hash(password):
+    return pwd_context.hash(password)
