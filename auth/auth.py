1c124786d218e136fb91e394fef20355c0b87dde
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:15:30 +0530

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -35,3 +35,22 @@ async def register_user(
     return user
 
 
+@router.post("/login")
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires.seconds
+        # Unresolved attribute reference 'username' for class 'bool'
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
