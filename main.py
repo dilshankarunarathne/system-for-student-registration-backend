77fd50e1daca618226c893c90b0a7f26b1aa43e1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:19:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
