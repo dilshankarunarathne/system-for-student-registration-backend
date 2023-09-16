3bcf125982299ff0be479f7ef776fb7a6558dfd2
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:17:18 +0530

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,2 +1,12 @@
 
+"""
+    authorization function middleware
+"""
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/login")
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
