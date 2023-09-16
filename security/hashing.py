1b867f6ab104e47e663ce9dec61b9b95e846c104
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:20:38 +0530

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -13,3 +13,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
