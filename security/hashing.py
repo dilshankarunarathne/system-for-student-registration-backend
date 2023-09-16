8f59adec92a1611dfa9a0943123e6dea5cab4fe8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:20:58 +0530

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -19,4 +19,12 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-
+def create_access_token(data: dict, expires_delta: int = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
