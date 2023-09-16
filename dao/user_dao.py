4f1452e05bb2c745953463d5677caa865b411ff6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:24:21 +0530

    Update user_dao.py

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -4,3 +4,9 @@
 """
 
 class UserDAO:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.cnx = None
