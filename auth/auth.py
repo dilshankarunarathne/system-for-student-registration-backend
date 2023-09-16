cdf2448941a38cfc4169cb712699ec4e751bf853
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:14:18 +0530

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -11,3 +11,25 @@ router = APIRouter(
 )
 
 
+@router.post("/register")
+async def register_user(
+        username: str = Form(...),
+        email: str = Form(...),
+        password: str = Form(...),
+        is_adviser: bool = Form(...),
+):
+    if user_exists(username):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists",
+        )
+    hashed_password = get_password_hash(password)
+    user = UserInDB(
+        id=get_next_avail_id(),
+        username=username,
+        email=email,
+        hashed_password=hashed_password,
+        is_adviser=is_adviser,
+    )
+    add_new_user(user)
+    return user
