7e941a4660ae48124d349a653d7363e15b2d4e0d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:18:12 +0530

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -23,4 +23,18 @@ def authenticate_user(username: str, password: str):
     return user
 
 
-
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    if is_token_blacklisted(token):
+        raise credentials_exception
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
