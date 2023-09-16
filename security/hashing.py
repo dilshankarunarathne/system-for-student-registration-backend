14edd53ece0c8201ca45dfb991df4322765ff631
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-16 11:20:36 +0530

    Update hashing.py

--- security/hashing.py
+++ security/hashing.py
@@ -11,4 +11,5 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
