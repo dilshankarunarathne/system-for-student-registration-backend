8ae350e2ddbc5be42b4b3bb89f8d1a1f8a33f905
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 10:50:50 +0530

    Update router.py

--- routes/attendance/router.py
+++ routes/attendance/router.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.post("/facerec")
-def mark_by_face(
+def async mark_by_face(
         image: UploadFile = File(...),
         token: str = Depends(oauth2_scheme)
 ):
