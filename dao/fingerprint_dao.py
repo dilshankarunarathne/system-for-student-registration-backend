eb118df56e4bfcac95abd578e9b0490db2b371d0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 12:47:41 +0530

    Update fingerprint_dao.py

--- dao/fingerprint_dao.py
+++ dao/fingerprint_dao.py
@@ -3,4 +3,9 @@
 """
 
 class FingerprintDAO:
-    
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.cnx = None
