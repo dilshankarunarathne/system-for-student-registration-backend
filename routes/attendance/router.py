4a40d3f8560ab37d89cbf3686bd54b181708b308
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 12:25:33 +0530

    Update router.py

--- routes/attendance/router.py
+++ routes/attendance/router.py
@@ -23,7 +23,7 @@ async def mark_by_id(
 ):
     """
         provides the ability to mark the attendance of a student via fingerprint
-        :param barcode_data: binary
+        :param barcode_data: binary data of the barcode extracted from the scanner
         :param token: oauth2 token
         :return: redirect to /attendance
         """
