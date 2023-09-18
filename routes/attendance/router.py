77496e417fdc6a94565d221086d0150063f768d1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 10:41:46 +0530

    Update router.py

--- routes/attendance/router.py
+++ routes/attendance/router.py
@@ -4,4 +4,8 @@
     routers for attendance marking
 """
 
-
+router = APIRouter(
+    prefix="/api/attendance",
+    tags=["attendance"],
+    responses={404: {"description": "The requested page was not found"}},
+)
