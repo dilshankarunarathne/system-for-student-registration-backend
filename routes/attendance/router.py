dedf54d15bbf6d0b9f8bc4e5812d8bf5c2c854e7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 10:52:04 +0530

    Update router.py

--- routes/attendance/router.py
+++ routes/attendance/router.py
@@ -31,4 +31,5 @@ async def mark_by_face(
     nparray = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
 
-    # TODO evaluate image 
+    # TODO evaluate image
+    # TODO mark attendance if evaluated for a user
