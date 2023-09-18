98536792d33e7712edde4b03e670ccf54377b1c5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 13:13:55 +0530

    Update fingerprint_service.py

--- services/fingerprint_service.py
+++ services/fingerprint_service.py
@@ -14,8 +14,7 @@ except Exception as e:
 
 
 def add_fingerprint_to_db(fingerprint_data):
-    # TODO add fingerprint data to the database
-    pass
+    
 
 
 def check_fingerprint(fingerprint_data):
