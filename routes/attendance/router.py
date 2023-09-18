06c200942abb45af3e218817d61a43391922caf0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 10:55:09 +0530

    Update router.py

--- routes/attendance/router.py
+++ routes/attendance/router.py
@@ -21,7 +21,12 @@ async def mark_by_face(
         image: UploadFile = File(...),
         token: str = Depends(oauth2_scheme)
 ):
+    """
     
+    :param image:
+    :param token:
+    :return:
+    """
     if image.content_type != "image/jpeg":
         return "Only jpeg images are supported"
 
