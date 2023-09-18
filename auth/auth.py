6c25f4fb4fbd5d2fabbc0c3d18758595ba461adc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 19:54:09 +0530

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -28,7 +28,6 @@ async def register_user(
         email: str = Form(...),
         password: str = Form(...),
         is_instructor: bool = Form(...),
-        is_adviser=None
 ):
     if user_exists(username):
         raise HTTPException(
