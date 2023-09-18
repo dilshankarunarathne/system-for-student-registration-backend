217da151cd4172af661f77451190a85422dca7e3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 12:59:05 +0530

    Update fingerprint_dao.py

--- dao/fingerprint_dao.py
+++ dao/fingerprint_dao.py
@@ -47,7 +47,7 @@ class FingerprintDAO:
         self.cnx.commit()
         cursor.close()
 
-    def check_fingerprint(self, fingerprint_data):
+    def get_all_fingerprints:
 
     def get_next_id(self) -> int:
         cursor = self.cnx.cursor()
