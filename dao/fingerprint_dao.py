dd0da6656a6f8ae089da5f906748080fecbca13b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 12:47:17 +0530

    Update fingerprint_dao.py

--- dao/fingerprint_dao.py
+++ dao/fingerprint_dao.py
@@ -1,3 +1,3 @@
 """
-    middleware for accessing the student database and performing CRUD operations on the user table
+    middleware for accessing the student database and performing CRUD operations on the fingerprint table
 """
