84f9c6ca2ba3b88b092f8e8409555bc62e835dcf
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 13:04:14 +0530

    Delete database_service.py

--- services/database_service.py
+++
@@ -1,8 +0,0 @@
-from dao.user_dao import UserDAO
-
-dao = UserDAO(host="localhost", user="root", password="", database="enad")
-try:
-    dao.connect()
-    print("EnAdDB connection successful")
-except Exception as e:
-    print("User DB connection error:", e)
