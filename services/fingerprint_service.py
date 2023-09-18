63228fd5a2e8aed5f75b9bd4cf76fe7599364418
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-18 13:13:10 +0530

    Update fingerprint_service.py

--- services/fingerprint_service.py
+++ services/fingerprint_service.py
@@ -22,7 +22,6 @@ def check_fingerprint(fingerprint_data):
     student_id = check_against_all(fingerprint_data)
 
 
-
 def check_against_all(fingerprint_data):
     fingerprints = dao.get_all_fingerprints()
 
