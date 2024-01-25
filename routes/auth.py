aa71b48da3063bd86ad8abae474ee4a04a68c54a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-26 03:07:51 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -65,14 +65,5 @@ async def login_for_access_token(
 
 @router.post("/logout")
 async def logout(token: str = Depends(oauth2_scheme)):
-    """
-    The endpoint for logging out a user
-
-    Args:
-        token (oauth2 bearer token): the token for the user
-
-    Returns:
-        (dict) The message for logging out
-    """
     blacklist_token(token)
     return {"message": "Successfully logged out"}
