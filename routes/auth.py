f8d001a4bbfbd02b0df20c14fe2f6d0dc7f05e40
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 15:56:51 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -52,7 +52,7 @@ async def login_for_access_token(
         # Unresolved attribute reference 'username' for class 'bool'
     )
 
-    return {"access token": access_token, "token type": "bearer"}
+    return {"operation": "successful", "access token": access_token, "token type": "bearer"}
 
 
 @router.post("/logout")
