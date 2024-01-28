0aaabcfb33e377691990bd2154092e16ea8ff42c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 13:05:54 +0530

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -12,12 +12,5 @@ def user_exists(username: str) -> bool:
     return True
 
 
-def get_next_avail_id() -> int:
-    last_id = dao.get_last_user_id()
-    if last_id is None:
-        return 1
-    return last_id + 1
-
-
 def get_user(username: str):
     return dao.get_user_by_username(username)
