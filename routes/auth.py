f38ca63f8036776aec6133e89ecab086c32e2255
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 15:57:08 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -36,7 +36,6 @@ async def register_user(
 @router.post("/login")
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-        role: str = Form(...)
 ):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
