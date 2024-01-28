8bf99149cb668f0efa7930efec916ed84467ea82
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 14:21:26 +0530

    Update attendance_dao.py

--- dao/attendance_dao.py
+++ dao/attendance_dao.py
@@ -3,23 +3,23 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://cluster-user:WSamCRFjm47IjoNT@cluster0.nwgoyl7.mongodb.net/")
 
 mydb = client["studentinfo"]
-user_collection = mydb['attendance']
+attendance_collection = mydb['attendance']
 
 
 def query_attendance_info_for_student(student_id):
     filt = {'student_id': student_id}
-    attendance = user_collection.find_one(filt)
+    attendance = attendance_collection.find_one(filt)
     return attendance
 
 
 def query_attendance_info_for_class(course_id, date):
     filt = {'course_id': course_id, 'date': date}
-    attendance = user_collection.find_one(filt)
+    attendance = attendance_collection.find_one(filt)
     return attendance
 
 
 def clear_all_records():
-    user_collection.delete_many({})
+    attendance_collection.delete_many({})
 
 
 if __name__ == '__main__':
