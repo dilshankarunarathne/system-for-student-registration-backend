1e13e2ce7969e68678f0e23664de21a4a820a75c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 14:43:46 +0530

    Update attendance_calculator.py

--- middleware/attendance_calculator.py
+++ middleware/attendance_calculator.py
@@ -6,6 +6,7 @@ def get_percentage(attendance_data):
     final_percentage = sum(current_percentage) / len(current_percentage)
     if final_percentage < 0.80:
         ext = 0.80 - final_percentage
+        ext_type = "shortage"
     elif final_percentage > 0.80:
         ext = final_percentage - 0.80
 
