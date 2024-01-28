bb361e69e0b4aa385a8e038c906bdfe38dc6ecbf
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 14:33:04 +0530

    Update student_registration.py

--- middleware/student_registration.py
+++ middleware/student_registration.py
@@ -1,9 +1,2 @@
 def register_student(request):
-    if request.method == 'POST':
-        form = StudentRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('student_list')
-    else:
-        form = StudentRegistrationForm()
-    return render(request, 'student_registration.html', {'form': form})
+    
