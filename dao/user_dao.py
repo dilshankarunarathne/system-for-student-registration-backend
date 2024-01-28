8063fe5b149cbaafdf91ea035a066b01036f6967
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 12:54:52 +0530

    Update user_dao.py

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -5,7 +5,7 @@ import base64
 client = pymongo.MongoClient("mongodb+srv://cluster-user:WSamCRFjm47IjoNT@cluster0.nwgoyl7.mongodb.net/")
 
 mydb = client["studentinfo"]
-studentinfo_collection = mydb['student']
+studentinfo_collection = mydb['user']
 
 
 
