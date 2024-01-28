8422cfc2f986da53cf3a78f667ad584199d6b673
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 17:54:19 +0530

    Update token_dao.py

--- dao/token_dao.py
+++ dao/token_dao.py
@@ -3,14 +3,14 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://cluster-user:WSamCRFjm47IjoNT@cluster0.nwgoyl7.mongodb.net/")
 
 mydb = client["studentinfo"]
-user_collection = mydb['blacklist']
+blacklist_collection = mydb['blacklist']
 
 
 def blacklist_token(token):
-    user_collection.insert_one({'token': token})
+    blacklist_collection.insert_one({'token': token})
 
 
 def _get_next_id():
-    last_user = user_collection.find().sort('id', -1).limit(1)
+    last_user = blacklist_collection.find().sort('id', -1).limit(1)
     for user in last_user:
         return user['id'] + 1
