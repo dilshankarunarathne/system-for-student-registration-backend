3822c0ce8a8c919fc6a98b7aeb1520d8530218d5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:18:33 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -6,7 +6,7 @@ mydb = client["studentinfo"]
 user_collection = mydb['student']
 
 
-def insert_student( student_name, student_email, student_course, student_year, student_semester):
+def insert_student(student_name, student_email, student_course, student_year, student_semester):
     student = {
         'id': _id,
         'name': student_name,
