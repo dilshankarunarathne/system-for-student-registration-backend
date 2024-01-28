4650b8430fa108ef3f72920292e02f68c390d981
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 15:55:27 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,11 +1,8 @@
 from datetime import timedelta
-
 from typing import Annotated
 
 from fastapi import APIRouter, Form, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-
-from models.user_model import UserInDB
 
 from auth.authorize import authenticate_user, oauth2_scheme
 from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
