f7579b12bf3b4e06ea27bc483f485881608f1415
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 17:58:26 +0530

    Update auth_route.py

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -6,6 +6,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from auth.authorize import authenticate_user, oauth2_scheme
 from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from dao.token_dao import blacklist_token
 from services.user_service import user_exists, add_new_user
 
 router = APIRouter(
