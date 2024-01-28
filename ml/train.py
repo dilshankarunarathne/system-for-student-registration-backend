884717a155f43fe65bce20bd2d62bd7e5411b0d8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 12:38:34 +0530

    Create train.py

---
+++ ml/train.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import pickle
+import face_recognition
+
+DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+
+Path("training").mkdir(exist_ok=True)
+Path("output").mkdir(exist_ok=True)
+Path("validation").mkdir(exist_ok=True)
+
+
+def encode_known_faces(
+        model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
+) -> None:
+    names = []
+    encodings = []
+    for filepath in Path("training").glob("*/*"):
+        name = filepath.parent.name
+        image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+    name_encodings = {"names": names, "encodings": encodings}
+    with encodings_location.open(mode="wb") as f:
+        pickle.dump(name_encodings, f)
+
+
+encode_known_faces()
