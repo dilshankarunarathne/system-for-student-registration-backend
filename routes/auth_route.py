b8d3a550262a67169fe96bb179018b315a36b123
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:38:12 +0530

    Update auth_route.py

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -46,7 +46,7 @@ async def register_student(
 
     hashed_password = get_password_hash(password)
 
-    return add_new_student(username, hashed_password, email, role)
+    return add_new_student(username, hashed_password, email, student_name, student_year, reg_no)
 
 
 @router.post("/register-lecturer")
