9a4d4acbc541ac0d118b1bfbc099510b7b9a03f8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 18:55:22 +0530

    Update attendance_route.py

--- routes/attendance_route.py
+++ routes/attendance_route.py
@@ -31,7 +31,7 @@ async def mark_single_attendance(
     attendance = mark_attendance(student_id, course_id, date, attended_time, total_time)
     print(attendance)
 
-    return {"message": "operation successful", "attendance marked": attendance}
+    return {"message": "operation successful", "attendance marked": attendance["id"]}
 
 
 @router.post("/clear")
