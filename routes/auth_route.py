de79ad4a70e91eb16684a1756d62cfa8b41c75d1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:37:16 +0530

    Update auth_route.py

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -26,6 +26,17 @@ async def register_student(
         reg_no: str = Form(...),
         token: str = Depends(oauth2_scheme)
 ):
+    user = await get_current_user(token)
+
+    if user is None:
+        raise credentials_exception
+
+    if user["role"] != "lecturer":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Only lecturers can clear attendance records",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     
     if user_exists(username):
         raise HTTPException(
