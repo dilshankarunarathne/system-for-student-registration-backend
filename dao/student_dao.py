f40bc60b277bbddf8d727a65cd6c0db252f81234
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:16:17 +0530

    Update student_dao.py

---
+++ dao/student_dao.py
@@ -0,0 +1,7 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://cluster-user:WSamCRFjm47IjoNT@cluster0.nwgoyl7.mongodb.net/")
+
+mydb = client["studentinfo"]
+user_collection = mydb['student']
+
