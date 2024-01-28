4e58cad394bc42aaec5c1e2f678b403a56db2040
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 16:20:20 +0530

    Create auth_route.py

---
+++ routes/auth_route.py
@@ -0,0 +1,60 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Form, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from auth.authorize import authenticate_user, oauth2_scheme
+from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
+from services.user_service import user_exists, add_new_user
+
+router = APIRouter(
+    prefix="/api/auth",
+    tags=["auth"],
+    responses={404: {"description": "The requested url was not found"}},
+)
+
+
+@router.post("/register")
+async def register_user(
+        username: str = Form(...),
+        email: str = Form(...),
+        password: str = Form(...),
+        role: str = Form(...)
+):
+    if user_exists(username):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists",
+        )
+
+    hashed_password = get_password_hash(password)
+
+    return add_new_user(username, hashed_password, email, role)
+
+
+@router.post("/login")
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires.seconds
+        # Unresolved attribute reference 'username' for class 'bool'
+    )
+
+    return {"operation": "successful", "access token": access_token, "token type": "bearer"}
+
+
+@router.post("/logout")
+async def logout(token: str = Depends(oauth2_scheme)):
+    blacklist_token(token)
+    return {"message": "Successfully logged out"}
