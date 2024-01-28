6125d89444ee3bcdf8600815d2984d23705e7791
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 23:20:52 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -6,6 +6,9 @@ mydb = client["studentinfo"]
 student_collection = mydb['student']
 
 
+
+
+
 def get_student_info_by_uid(_u_id):
     filt = {'u_id': _u_id}
     student = student_collection.find_one(filt)
