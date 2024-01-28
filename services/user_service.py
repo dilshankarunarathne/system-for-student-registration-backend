e09c660432a6da099b842e8e39c1d68226f7b6a5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 18:07:49 +0530

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -8,7 +8,12 @@ def add_new_user(username: str, password_hash: str, email: str, role: str):
 
     user = insert_user(username, password_hash, email, role)
     if user:
-        return {"message": "operation successful", "user": user['username'], "id": user['id']}
+        return \
+            {
+                "message": "operation successful",
+                "user": user['username'],
+                "id": user['id']
+            }
 
 
 def user_exists(username: str) -> bool:
