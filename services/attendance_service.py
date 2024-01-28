03517a39f035159f32c2e94152ab0352aec0887c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 14:20:16 +0530

    Update attendance_service.py

--- services/attendance_service.py
+++ services/attendance_service.py
@@ -5,4 +5,4 @@ def get_attendance_info_for_student(student_id):
     return query_attendance_info_for_student(student_id)
 
 
-def get_attendance_info_for_class(course_id, date):
+def get_attendance_info_for_lecture(course_id, date):
