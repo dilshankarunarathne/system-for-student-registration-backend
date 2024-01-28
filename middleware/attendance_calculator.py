eb9454401b3ca6a4b6f2a85f17ede6fb11ca1268
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 21:42:11 +0530

    Update attendance_calculator.py

--- middleware/attendance_calculator.py
+++ middleware/attendance_calculator.py
@@ -1,7 +1,8 @@
 def get_percentage(attendance_data):
     current_percentage = []
     for i in attendance_data:
-        current_percentage.append(i['attended_time'] / i['total_time'])
+        a_time = int(i['attended_time'])
+        current_percentage.append(a_time / i['total_time'])
 
     final_percentage = sum(current_percentage) / len(current_percentage)
     if final_percentage < 0.80:
