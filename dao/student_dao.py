ee60d8ca3ca47b786b2f81010ffa3fc5aacc5217
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:19:51 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -19,7 +19,7 @@ def insert_student(student_name, student_year, reg_no, _u_id):
     return student
 
 
-def get_student_info_by_id(student_id):
-    filt = {'id': student_id}
-    student = user_collection.find_one(filt)
-    return student
+def _get_next_student_id():
+    last_student = user_collection.find().sort('id', -1).limit(1)
+    for student in last_student:
+        return student['id'] + 1
