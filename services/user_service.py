09e58def4ef7c56367ff3de8c441e8cee5c95ad4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:24:27 +0530

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -17,8 +17,7 @@ def add_new_student(username: str, password_hash: str, email: str):
     # TODO: use middleware
     # register_student()
 
-    role = 'student'
-    _add_new_user()
+    _add_new_user(username, password_hash, email, 'student')
     pass
 
 
