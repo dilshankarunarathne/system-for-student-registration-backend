6dc7ffd6ff3b765c0ca1cc37ce08dc820a48476e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 21:37:41 +0530

    Update attendance_route.py

--- routes/attendance_route.py
+++ routes/attendance_route.py
@@ -76,6 +76,9 @@ async def attendance_info_for_class(
     if await get_current_user(token) is None:
         raise credentials_exception
 
-    # TODO: use middleware to get stats
+    get_attendance_info_for_lecture(course_id, date)
 
-    return get_attendance_info_for_lecture(course_id, date)
+    # TODO: use middleware to get stats
+    get_percentage()
+
+    return
