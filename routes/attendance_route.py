e8c2a3d7fdca385aaf27258b949b0ee24edddcd6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 16:42:02 +0530

    Update attendance_route.py

--- routes/attendance_route.py
+++ routes/attendance_route.py
@@ -6,9 +6,11 @@ router = APIRouter(
     responses={404: {"description": "The requested url was not found"}},
 )
 
+
 @router.post("/student")
 async def get_attendance_info_for_student(student_id):
     pass
+
 
 @router.post("/lecture")
 async def get_attendance_info_for_class(course_id, date):
