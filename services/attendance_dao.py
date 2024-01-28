18a647cf97bb1d668e2d0cf5820ab2dbd5a318ad
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 14:16:12 +0530

    Update attendance_dao.py

--- services/attendance_dao.py
+++ services/attendance_dao.py
@@ -6,7 +6,10 @@ mydb = client["studentinfo"]
 user_collection = mydb['attendance']
 
 
-def 
+def get_attendance_info_for_student(student_id):
+    filt = {'student_id': student_id}
+    attendance = user_collection.find_one(filt)
+    return attendance
 
 
 if __name__ == '__main__':
