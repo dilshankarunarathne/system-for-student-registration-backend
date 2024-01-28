c679dee9d67ed927ca55a268e3abcda0588b1f31
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 20:47:51 +0530

    Update attendance_route.py

--- routes/attendance_route.py
+++ routes/attendance_route.py
@@ -52,7 +52,7 @@ async def clear_all_records(
     cur = clear_all_records()
 
     # TODO: create placeholder _id=1 document
-    
+
     return {"message": "operation successful", "attendance cleared": cur}
 
 
