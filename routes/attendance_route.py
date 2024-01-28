e47f117f942441ce7741c34aec04c4d23f1d2fa4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-28 17:07:59 +0530

    Update attendance_route.py

--- routes/attendance_route.py
+++ routes/attendance_route.py
@@ -25,6 +25,7 @@ async def attendance_info_for_student(
 async def attendance_info_for_class(
         course_id: str = Form(...),
         date: str = Form(...),
+        token: str = Depends(oauth2_scheme)
 ):
     if get_current_user(token) is None:
         raise credentials_exception
