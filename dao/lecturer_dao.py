d86ed1b77fbd4b8c78b07db9efb39614323631f5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 06:44:38 +0530

    Update lecturer_dao.py

--- dao/lecturer_dao.py
+++ dao/lecturer_dao.py
@@ -7,6 +7,9 @@ mydb = client["studentinfo"]
 lecturer_collection = mydb['lecturer']
 
 
+
+
+
 def query_lecturer_info_by_uid(_u_id):
     filt = {'u_id': _u_id}
     lecturer = lecturer_collection.find_one(filt)
