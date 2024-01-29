5e14a1017c1d6ead159b4ca0570fd564392bffee
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 07:25:26 +0530

    Update class_service.py

--- services/class_service.py
+++ services/class_service.py
@@ -5,5 +5,5 @@ def add_new_class(course_id, date, start_time, duration):
     return insert_class_info(course_id, date, start_time, duration)
 
 
-def get_class_info(course_id):
-    return query_class_info(course_id)
+def get_class_info(class_id):
+    return query_class_info(class_id)
