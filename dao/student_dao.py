4cd89f0d292650ce5db16a598146e11a2bf17d0b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 05:30:17 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -17,12 +17,14 @@ def get_all_students():
 def query_student_info_by_id(_id):
     filt = {'id': _id}
     student = student_collection.find_one(filt)
+    student['_id'] = str(student['_id'])
     return student
 
 
 def query_student_info_by_uid(_u_id):
     filt = {'u_id': _u_id}
     student = student_collection.find_one(filt)
+    student['_id'] = str(student['_id'])
     return student
 
 
