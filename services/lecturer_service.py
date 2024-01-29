da2565d4c62eafc8969770628c5c82635e7793ac
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 06:44:42 +0530

    Update lecturer_service.py

--- services/lecturer_service.py
+++ services/lecturer_service.py
@@ -2,4 +2,4 @@ from dao.lecturer_dao import query_lecturer_info_by_uid
 
 
 def get_lecturer_by_id(lecturer_id):
-    return query_lecturer_info_by_uid(lecturer_id)
+    return query_lecturer_info_by_id(lecturer_id)
