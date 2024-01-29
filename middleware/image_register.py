aa92e672f0f734d3478de4136f56844838a23778
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 16:09:20 +0530

    Update image_register.py

--- middleware/image_register.py
+++ middleware/image_register.py
@@ -8,4 +8,6 @@ def save_image(frame, class_name, count):
 
 
 def store_image_model_info(class_name, student_id):
-    
+    with open("training_info.txt", "a") as f:
+        f.write(f"{class_name} {student_id}\n")
+    print(f"Student {class_name} {student_id} info saved successfully")
