fc08aac7301797846eaed036b3509fd66513435a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 05:45:52 +0530

    Update course_route.py

--- routes/course_route.py
+++ routes/course_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 router = APIRouter(
-    prefix="/api/student",
-    tags=["attendance"],
+    prefix="/api/course",
+    tags=["course"],
     responses={404: {"description": "The requested url was not found"}},
 )
