6676757ca24ae9fa23db12d3a4b3fb1b913d5530
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 05:57:44 +0530

    Update course_dao.py

--- dao/course_dao.py
+++ dao/course_dao.py
@@ -19,8 +19,10 @@ def query_all_courses():
 def query_course_by_id(_id):
     cid = int(_id)
     filt = {'id': cid}
+    print(filt)
     course = course_collection.find_one(filt)
     if course is None:
+        print("Course not found")
         return None
     course['_id'] = str(course['_id'])
     return course
