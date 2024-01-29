b5c7fb9d17c21fb073170a20406c3da84f370063
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 16:07:19 +0530

    Update image_register.py

--- middleware/image_register.py
+++ middleware/image_register.py
@@ -1,4 +1,8 @@
+import cv2
+
+
 def save_image(frame, class_name, count):
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imwrite(f"training/{class_name}/{class_name}_{count}.jpg", frame)
     print("Image saved successfully")
+    
