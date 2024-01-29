d650ef81b024a94d3ebc2b5334660e8f03a393a3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 05:43:27 +0530

    Update course_dao.py

--- dao/course_dao.py
+++ dao/course_dao.py
@@ -4,5 +4,7 @@ from bson.objectid import ObjectId
 client = pymongo.MongoClient("mongodb+srv://cluster-user:WSamCRFjm47IjoNT@cluster0.nwgoyl7.mongodb.net/")
 
 mydb = client["studentinfo"]
-course_collection = mydb['student']
+course_collection = mydb['course']
 
+
+def get_all_courses():
