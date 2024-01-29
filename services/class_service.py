92fe6ded4c44d4ea8e4529f4c8b9c9074e2facc8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 07:22:30 +0530

    Update class_service.py

--- services/class_service.py
+++ services/class_service.py
@@ -2,7 +2,7 @@ from dao.class_dao import insert_class_info, query_class_info
 
 
 def add_new_class(course_id, date, start_time, duration):
-    insert_class_info(course_id, date, start_time, duration)
+    return insert_class_info(course_id, date, start_time, duration)
 
 
 def get_class_info(course_id):
